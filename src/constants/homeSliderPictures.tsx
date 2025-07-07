d79bbb69e900d3e407a984e74d5d8a4026b1e850
Iva Leva <M.Sc.Iva.Leva@gmail.com>
2025-07-07 19:08:37 +0200

    Update home slider

--- src/constants/homeSliderPictures.tsx
+++ src/constants/homeSliderPictures.tsx
@@ -1,9 +1,7 @@
 import netKafe from "../assets/img/homeSliderPictures/NetKafe.jpg";
-import pizza from "../assets/img/homeSliderPictures/pizzaNetKafe2.jpg";
-import pizza2 from "../assets/img/homeSliderPictures/A7.jpg";
-import pizza3 from "../assets/img/homeSliderPictures/pizza3.jpg";
+import pizza from "../assets/img/homeSliderPictures/pizza.jpg";
 import lasagne from "../assets/img/homeSliderPictures/lasagne.jpg";
-import salad from "../assets/img/homeSliderPictures/saladNetKafe2.jpg";
+import salad from "../assets/img/homeSliderPictures/salad.jpg";
 
 import sweets from "../assets/img/homeSliderPictures/sweets.jpg";
 import blueberry from "../assets/img/homeSliderPictures/blueberry.jpg";
@@ -11,54 +9,27 @@ import blueberry from "../assets/img/homeSliderPictures/blueberry.jpg";
 import football from "../assets/img/homeSliderPictures/football.jpg";
 import beer from "../assets/img/homeSliderPictures/beer.jpg";
 import frenchFries from "../assets/img/homeSliderPictures/frenchFries.jpg";
-// import juice from "../assets/img/homeSliderPictures/juice.jpg";
-import juice from "../assets/img/homeSliderPictures/IMG_20250630_183951.jpg";
+import juice from "../assets/img/homeSliderPictures/juice.jpg";
 
-import beer1 from "../assets/img/homeSliderPictures/beer1 IMG_20250630_182021.jpg";
-import beer2 from "../assets/img/homeSliderPictures/beer2b.jpg";
-import beer3 from "../assets/img/homeSliderPictures/beer3b IMG_20250630_182142.jpg";
-import drinksJuices from "../assets/img/homeSliderPictures/drinksJuices1.jpg";
-import drinksWines from "../assets/img/homeSliderPictures/drinksWines.jpg";
-import vino1 from "../assets/img/homeSliderPictures/vino1d IMG_20250630_182918.jpg";
-import vino2 from "../assets/img/homeSliderPictures/vino2m IMG_20250630_182904.jpg";
-import limo1 from "../assets/img/homeSliderPictures/limo1b IMG_20250630_184247_1.jpg";
 import aiFood from "../assets/img/homeSliderPictures/aiFood.jpg";
 import aiIceCream from "../assets/img/homeSliderPictures/aiIceCream.jpg";
 import aiFootball from "../assets/img/homeSliderPictures/aiFootball.jpg";
 import aiSnacks from "../assets/img/homeSliderPictures/aiSnacks.jpg";
-import A4 from "../assets/img/homeSliderPictures/A4.jpg";
-import b29 from "../assets/img/homeSliderPictures/IMG_20250630_183829.jpg";
-import g100 from "../assets/img/homeSliderPictures/100_gama_2024.png";
-import ham from "../assets/img/homeSliderPictures/ham.jpg";
-import ham3 from "../assets/img/homeSliderPictures/ham3c.jpg";
-import hs from "../assets/img/homeSliderPictures/h+s2.jpg";
-import wrap from "../assets/img/homeSliderPictures/wrap.jpg";
-import wrap3 from "../assets/img/homeSliderPictures/wrap3b.jpg";
-import sn1 from "../assets/img/homeSliderPictures/sn1.jpg";
-import ice1 from "../assets/img/homeSliderPictures/ice1d.jpg";
-import ice2 from "../assets/img/homeSliderPictures/ice2.jpg";
-import ice3 from "../assets/img/homeSliderPictures/ice3c.jpg";
-import ice4 from "../assets/img/homeSliderPictures/ice4b.jpg";
-import food1 from "../assets/img/homeSliderPictures/Food1.jpg";
-import food2 from "../assets/img/homeSliderPictures/Food2c.jpg";
-import olives from "../assets/img/homeSliderPictures/olives.jpg";
-import i12 from "../assets/img/homeSliderPictures/i12.jpg";
 
 export const sliderPictures = [
   netKafe,
-  // aiFood,
+  aiFood,
   pizza,
+  lasagne,
   salad,
-  ice4,
-  ice1,
-  i12,
+  aiIceCream,
   football,
   aiFootball,
   beer,
   frenchFries,
-  olives,
-  beer3,
-  vino2,
-  limo1,
+  aiSnacks,
+  sweets,
+  blueberry,
+  juice,
   netKafe,
 ];
